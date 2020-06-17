b06a240dda39a54ef75af2add5e9ade1be06003b
Pavel Ovechkin <pas7273@gmail.com>
2020-06-17 15:34:11 +0300

    Updates

---
+++ precache-manifest.63fab13736b19628995d1e7173bbca94.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2081ecbbcbb46b9b98cb975f2f738587",
+    "url": "/yp-graduate-work-react/index.html"
+  },
+  {
+    "revision": "c7e60f6f3b2c9469e24b",
+    "url": "/yp-graduate-work-react/static/css/main.283b52fc.chunk.css"
+  },
+  {
+    "revision": "02e8b31ba2c7114b9725",
+    "url": "/yp-graduate-work-react/static/js/2.ad5a5035.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/yp-graduate-work-react/static/js/2.ad5a5035.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7e60f6f3b2c9469e24b",
+    "url": "/yp-graduate-work-react/static/js/main.565fb726.chunk.js"
+  },
+  {
+    "revision": "6a9862f85867e66dbd43",
+    "url": "/yp-graduate-work-react/static/js/runtime-main.9569c54e.js"
+  },
+  {
+    "revision": "a731c6ae30c54fd315686ce1a32b9db9",
+    "url": "/yp-graduate-work-react/static/media/background.a731c6ae.png"
+  },
+  {
+    "revision": "ff5d45de70e534330949a7b04801858f",
+    "url": "/yp-graduate-work-react/static/media/css.ff5d45de.svg"
+  },
+  {
+    "revision": "a3502a5e8d18142dcc90ef7b5cef6f22",
+    "url": "/yp-graduate-work-react/static/media/fb.a3502a5e.svg"
+  },
+  {
+    "revision": "83610e3ce913df9e9dc9d7185fd01d61",
+    "url": "/yp-graduate-work-react/static/media/github.83610e3c.svg"
+  },
+  {
+    "revision": "26561b73393d21bc44f2406cfa84ab7d",
+    "url": "/yp-graduate-work-react/static/media/html.26561b73.svg"
+  },
+  {
+    "revision": "11a4c1c3be7a138177b78db537766019",
+    "url": "/yp-graduate-work-react/static/media/js.11a4c1c3.svg"
+  },
+  {
+    "revision": "5af0def6d56262f9c3792d11b4e30f36",
+    "url": "/yp-graduate-work-react/static/media/not-found.5af0def6.svg"
+  },
+  {
+    "revision": "6422eac71f54ee5cd007dcf46fc0d22d",
+    "url": "/yp-graduate-work-react/static/media/roboto-medium.6422eac7.woff"
+  },
+  {
+    "revision": "58bf8e0bd1dd213bf745e9d748caf175",
+    "url": "/yp-graduate-work-react/static/media/roboto-regular.58bf8e0b.woff"
+  },
+  {
+    "revision": "594bcf8bf5ddbb7302400b1319270585",
+    "url": "/yp-graduate-work-react/static/media/robotoslab-regular.594bcf8b.woff"
+  },
+  {
+    "revision": "49d8d8285e8bc3047dac602de489bfb0",
+    "url": "/yp-graduate-work-react/static/media/sourcesanspro-regular.49d8d828.woff"
+  },
+  {
+    "revision": "2f25192ae5bfb8978ac2ec351ef17e03",
+    "url": "/yp-graduate-work-react/static/media/webpack.2f25192a.svg"
+  }
+]);

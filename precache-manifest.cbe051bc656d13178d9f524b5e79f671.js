c0374833009162f5099d31f9aabd165dc542c743
opv1 <pas7273@gmail.com>
2020-10-22 21:14:04 +0300

    Updates

---
+++ precache-manifest.cbe051bc656d13178d9f524b5e79f671.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3598b01a49b66705763bbfec1adabc85",
+    "url": "/analyzer-app-yp-react/index.html"
+  },
+  {
+    "revision": "d1e458f4992ad60843ca",
+    "url": "/analyzer-app-yp-react/static/css/2.5b3efbda.chunk.css"
+  },
+  {
+    "revision": "fadf60132285f81c688d",
+    "url": "/analyzer-app-yp-react/static/css/main.a45f31ff.chunk.css"
+  },
+  {
+    "revision": "d1e458f4992ad60843ca",
+    "url": "/analyzer-app-yp-react/static/js/2.fee252a4.chunk.js"
+  },
+  {
+    "revision": "44ba416b9a7217ca99c025d9f4dd1124",
+    "url": "/analyzer-app-yp-react/static/js/2.fee252a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fadf60132285f81c688d",
+    "url": "/analyzer-app-yp-react/static/js/main.27fed6cb.chunk.js"
+  },
+  {
+    "revision": "42aaf4b5563f6c43a55c",
+    "url": "/analyzer-app-yp-react/static/js/runtime-main.38a17dc0.js"
+  },
+  {
+    "revision": "a731c6ae30c54fd315686ce1a32b9db9",
+    "url": "/analyzer-app-yp-react/static/media/background.a731c6ae.png"
+  },
+  {
+    "revision": "ff5d45de70e534330949a7b04801858f",
+    "url": "/analyzer-app-yp-react/static/media/css.ff5d45de.svg"
+  },
+  {
+    "revision": "a3502a5e8d18142dcc90ef7b5cef6f22",
+    "url": "/analyzer-app-yp-react/static/media/fb.a3502a5e.svg"
+  },
+  {
+    "revision": "83610e3ce913df9e9dc9d7185fd01d61",
+    "url": "/analyzer-app-yp-react/static/media/github.83610e3c.svg"
+  },
+  {
+    "revision": "26561b73393d21bc44f2406cfa84ab7d",
+    "url": "/analyzer-app-yp-react/static/media/html.26561b73.svg"
+  },
+  {
+    "revision": "11a4c1c3be7a138177b78db537766019",
+    "url": "/analyzer-app-yp-react/static/media/js.11a4c1c3.svg"
+  },
+  {
+    "revision": "5af0def6d56262f9c3792d11b4e30f36",
+    "url": "/analyzer-app-yp-react/static/media/not-found.5af0def6.svg"
+  },
+  {
+    "revision": "6422eac71f54ee5cd007dcf46fc0d22d",
+    "url": "/analyzer-app-yp-react/static/media/roboto-medium.6422eac7.woff"
+  },
+  {
+    "revision": "58bf8e0bd1dd213bf745e9d748caf175",
+    "url": "/analyzer-app-yp-react/static/media/roboto-regular.58bf8e0b.woff"
+  },
+  {
+    "revision": "594bcf8bf5ddbb7302400b1319270585",
+    "url": "/analyzer-app-yp-react/static/media/robotoslab-regular.594bcf8b.woff"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/analyzer-app-yp-react/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/analyzer-app-yp-react/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/analyzer-app-yp-react/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/analyzer-app-yp-react/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "49d8d8285e8bc3047dac602de489bfb0",
+    "url": "/analyzer-app-yp-react/static/media/sourcesanspro-regular.49d8d828.woff"
+  },
+  {
+    "revision": "2f25192ae5bfb8978ac2ec351ef17e03",
+    "url": "/analyzer-app-yp-react/static/media/webpack.2f25192a.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/yp-graduate-work-react/precache-manifest.adbe1ed91aa062640d8e75a17c58817c.js"
+  "/analyzer-app-yp-react/precache-manifest.cbe051bc656d13178d9f524b5e79f671.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/yp-graduate-work-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/analyzer-app-yp-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
